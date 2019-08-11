a3317a83d046174c4599a4976b9c122c67aa2241
James Fanizza <jamesfanizza@gmail.com>
2019-08-11 17:35:20 +0000

    added gram detail page

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -8,6 +8,13 @@ class GramsController < ApplicationController
   def index
   end
 
+  def show
+    @gram = Gram.find_by_id(params[:id])
+      if @gram.blank?
+      render plain: 'Not Found :(', status: :not_found
+      end
+  end
+
   def create
     @gram = current_user.grams.create(gram_params)
     if @gram.valid?
@@ -22,4 +29,5 @@ class GramsController < ApplicationController
   def gram_params
     params.require(:gram).permit(:message)
   end
+
 end
